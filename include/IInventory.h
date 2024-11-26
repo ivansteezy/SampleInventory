6d39cee2e248ffa52e21a279a177d81a49a68bc8
Syro <network.9961@gmail.com>
2024-11-26 10:47:20 -0600

    Add operator overload for print/write

--- include/IInventory.h
+++ include/IInventory.h
@@ -4,13 +4,15 @@
 #include "IItem.h"
 
 namespace Inventory {
+	using InventoryMap = std::map<ItemType, std::pair<int, IItemPtr>>;
+	
 	class IInventory {
 	public:
 		virtual void addItem(IItemPtr item) = 0;
 		virtual void printInventory() const = 0;
 		virtual bool saveInventoryIntoFile(const std::string& fileName) = 0;
 		virtual bool loadInventoryFromFile(const std::string& fileName) = 0;
+		virtual InventoryMap getInventory() const = 0;
 	};
-
 	using IInventoryPtr = std::shared_ptr<IInventory>;
 }
--- include/Inventory.h
+++ include/Inventory.h
@@ -1,11 +1,12 @@
 #include <map>
 #include <iostream>
 #include <algorithm>
+#include <fstream>
 
 #include "IInventory.h"
 
 namespace Inventory {
-    class Inventory : public IInventory{
+    class Inventory : public IInventory, std::enable_shared_from_this<Inventory> {
     public:
 		virtual void addItem(IItemPtr item) override {
             auto it = mItems.find(item->getType());
@@ -17,19 +18,46 @@ namespace Inventory {
         }
 
 		virtual void printInventory() const override {
-            for(const auto& a : mItems) {
-                std::cout << "Cantidad: [" << a.second.first << "] Nombre: [" << a.second.second->getName() << "]\n"; // magia del polimorfismo
-            }
-
-            std::cout << std::endl;
+            std::cout << *this;
         }
 
 		virtual bool saveInventoryIntoFile(const std::string& fileName) override {
-            // to do...
+            std::ofstream file(fileName);
+            if (file.is_open()) {
+                file << *this;
+                file.close();
+                return true;
+            } else {
+                std::cerr << "Error: Can not open file to write inventory" << std::endl;
+                return false;
+            }
         }
 
 		virtual bool loadInventoryFromFile(const std::string& fileName) override {
-            // to do...
+            auto x  = fileName;
+            return true;
+        }
+
+        virtual InventoryMap getInventory() const override {
+            return mItems;
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, const Inventory& inventory) {
+            if (os.rdbuf() == std::cout.rdbuf()) {
+                for(const auto& a : inventory.getInventory()) {
+                    os << "Cantidad: [" << a.second.first 
+                       << "] Nombre: [" << a.second.second->getName() 
+                       << "]\n";
+                }
+                os << std::endl;
+            } else {
+                for(const auto& i : inventory.getInventory()) {
+                    os << i.second.first << "," 
+                       << i.second.second->getName() << "," 
+                       << static_cast<int>(i.second.second->getType());
+                }
+            }
+            return os;
         }
 
     private:
